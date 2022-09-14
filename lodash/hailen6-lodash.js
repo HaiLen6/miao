8f73e4fb36ce3c830e09c2e9daad9b1b8f23f2f1
HaiLen6 <hbdylwg@163.com>
2022-09-14 19:55:02 +0800

    upgrade a new file

--- lodash/hailen6-lodash.js
+++ lodash/hailen6-lodash.js
@@ -1,26 +1,37 @@
 var hailen6 = {
-chunk: function(ary, size = 1) {
-  var result = [];
-  var bracketsNumber = Math.ceil(ary.length / size);
+  chunk: function(ary, size = 1) {
+    var result = [];
+    var bracketsNumber = Math.ceil(ary.length / size);
 
-  var index = 0;
+    var index = 0;
 
-  for (let i = 0; i < bracketsNumber; i++) {
-    let ini = [];
-    for (let j = index; j < index + size; j++) {
-      if (ary[j] === undefined) {
-        continue;
-      } else {
-        ini.push(ary[j]);
+    for (let i = 0; i < bracketsNumber; i++) {
+      let ini = [];
+      for (let j = index; j < index + size; j++) {
+        if (ary[j] === undefined) {
+          continue;
+        } else {
+          ini.push(ary[j]);
+        }
+      }
+
+      index += size;
+      result.push(ini);
+    }
+
+    return result;
+  },
+
+  compact: function(ary) {
+    var result = [];
+
+    for (let i = 0; i < ary.length; i++) {
+      if (ary[i] !== false && ary[i] !== null && ary[i] != "" && ary[i] !== undefined && ary[i] !== NaN) {
+        result.push(ary[i]);
       }
     }
 
-    index += size;
-    result.push(ini);
+    return result;
   }
-
-  return result;
-}
-  // compact:
   // fill:
 }
