ddee5e0bc7a3deeddf10f4d7991bfd23468f00c0
HaiLen6 <hbdylwg@163.com>
2022-09-14 19:26:54 +0800

    upgrade a new file

--- lodash/hailen6-lodash.js
+++ lodash/hailen6-lodash.js
@@ -1,5 +1,26 @@
 var hailen6 = {
-  chunk:
-  compact:
-  fill:
+  chunk: function chunk(ary, size = 1) {
+    var result = [];
+    var bracketsNumber = Math.ceil(ary.length / size);
+
+    var index = 0;
+
+    for (let i = 0; i < bracketsNumber; i++) {
+      let ini = [];
+      for (let j = index; j < index + size; j++) {
+        if (ary[j] === undefined) {
+          continue;
+        } else {
+          ini.push(ary[j]);
+        }
+      }
+
+      index += size;
+      result.push(ini);
+    }
+
+    return result;
+  }
+  // compact:
+  // fill:
 }
